85bf199c79bb1842676192ee07ea430a6511a20d
ArpanBalpande <arpan.balpande22@gmail.com>
2019-07-09 19:38:04 +0530

    Changed comment in RunCal.ps1

--- RunCal.ps1
+++ RunCal.ps1
@@ -1,2 +1,2 @@
 Copy-Item 'C:\DeployTemp\drop\WindowsApp1\bin\Release\WindowsApp1.exe' 'C:\'
-Write-Host 'Successfully copied the EXE file to the root directory'
+Write-Host 'Successfully copied the EXE file to the C:\ directory'
