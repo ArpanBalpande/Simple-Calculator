be68f39ead56b2aebd09a2f10d7e6326d2f69f37
ArpanBalpande <arpan.balpande22@gmail.com>
2019-07-10 01:09:57 +0530

    changed RunCal.ps1

--- RunCal.ps1
+++ RunCal.ps1
@@ -1,2 +1,3 @@
 Copy-Item 'C:\DeployTemp\drop\WindowsApp1\bin\Release\WindowsApp1.exe' 'C:\'
+Copy-Item 'drop\WindowsApp1\bin\Release\WindowsApp1.exe' 'C:\'
 Write-Host 'Successfully copied the EXE file to the C:\ directory'
