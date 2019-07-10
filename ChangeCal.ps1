151bba96beb7973357b319c0c9b80a24f393e5c0
ArpanBalpande <arpan.balpande22@gmail.com>
2019-07-10 21:09:00 +0530

    changed changecal.ps1

--- ChangeCal.ps1
+++ ChangeCal.ps1
@@ -1,2 +1,2 @@
 Copy-Item 'WindowsApp1\bin\Release\WindowsApp1.exe' 'C:\'
-Write-Host 'Successfully updated the EXE file at the C:\ directory'
+Write-Host 'Successfully updated the EXE file at C:\ directory'
